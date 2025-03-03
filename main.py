2c6012fcf73a9453c8227b5218fa9356b2931a66
mouxtaris <dimouch@cs.ihu.gr>
2025-03-03 21:07:50 +0200

    main file

--- main.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-    host="sql.freedb.tech",
-    user="freedb_mouxtaris",
-    password="$K!wCD9jwgaQm$3",
-    database="freedb_touristCenter"
-)
-
