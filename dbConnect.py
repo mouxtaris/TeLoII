1c36e45bf89d5a9fce6c02cc8438589b12b954ad
mouxtaris <149571892+mouxtaris@users.noreply.github.com>
2025-03-19 00:36:02 +0200

    Tables trips and buses

--- dbConnect.py
+++ dbConnect.py
@@ -6,8 +6,9 @@ connection = mysql.connector.connect(
     passwd="cgpf79CiTP",
     database="sql7767099"
 )
-
 cursor = connection.cursor()
+# cursor.close()
+
 # cursor.execute("CREATE TABLE `sql7767099`.`trips` ( `trip_name` VARCHAR(30) , `trip_desc` VARCHAR(300),"
 #                " `trip_start` DATE, `trip_end` DATE , `trip_hotel` VARCHAR(20) ,"
 #                " `trip_cost` INT(7), `trip_activities` VARCHAR(300))")
@@ -25,12 +26,26 @@ cursor = connection.cursor()
 # print(cursor.fetchall())
 # cursor.execute("DROP TABLE IF EXISTS trips")
 
+# cursor.execute("ALTER TABLE `sql7767099`.`buses` ADD bus_id VARCHAR(7) FIRST")
+
 # def crowdTrips(trips):
-
-cursor.execute("CREATE TABLE `sql7767099`.`trips` ( `trip_name` VARCHAR(30) , `trip_desc` VARCHAR(300),"
-               " `trip_start` DATE, `trip_end` DATE ,"
-               " `trip_cost` INT(7), `trip_activities` VARCHAR(300))",)
-cursor.execute("CREATE TABLE `sql7767099`.`buses` (`model` VARCHAR(30), `bus_year` INT(7),`bus_km` INT(8), "
-               "`service_cost` FLOAT(10,2), `service_time` INT(3), `operation_cost` FLOAT(10,2))")
+# connection.commit()
 
 
+def createTables():
+    cursor.execute("CREATE TABLE `sql7767099`.`trips` (`trip_id` INT AUTO_INCREMENT PRIMARY KEY, `trip_name` VARCHAR(30) , `trip_desc` VARCHAR(300),"
+                   " `trip_start` DATE, `trip_end` DATE ,"
+                   " `trip_cost` INT(7), `trip_activities` VARCHAR(300))")
+    cursor.execute("CREATE TABLE `sql7767099`.`buses` (`bus_id` VARCHAR(8) PRIMARY KEY ,`model` VARCHAR(30), `bus_year` INT(7),`bus_km` INT(8), "
+        "`service_cost` FLOAT(10,2), `service_time` INT(3), `operation_cost` FLOAT(10,2))")
+
+
+def crowdTrips(trip_values):
+    query = """INSERT INTO `sql7767099`.`trips` 
+                   (`trip_name`, `trip_desc`, `trip_start`, `trip_end`, `trip_cost`, `trip_activities`) 
+                   VALUES (%s, %s, %s, %s, %s, %s)"""
+
+    cursor.execute(query, trip_values)
+    connection.commit()
+    cursor.close()
+    connection.close()
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import dbConnect
 
+trip = ['ibiza', 'den tha kanoume tipota', '2024-12-12', '2024-12-14', 500, 'polles']
 
+dbConnect.crowdTrips(trip)
+
+# trip_id` INT AUTO_INCREMENT PRIMARY KEY, `trip_name` VARCHAR(30) , `trip_desc` VARCHAR(300),"
+#                    " `trip_start` DATE, `trip_end` DATE ,"
+#                    " `trip_cost` INT(7), `trip_activities` VARCHAR(300))
 
 
