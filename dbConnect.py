47950065f0114363de3c126c9475afc8eb67e026
mouxtaris <149571892+mouxtaris@users.noreply.github.com>
2025-04-08 19:45:55 +0300

    bd link

--- dbConnect.py
+++ dbConnect.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+#https://www.phpmyadmin.co/db_structure.php?server=1&db=sql7767099
 connection = mysql.connector.connect(
     host="sql7.freesqldatabase.com	",
     user="sql7767099	",
@@ -7,30 +8,12 @@ connection = mysql.connector.connect(
     database="sql7767099"
 )
 cursor = connection.cursor()
-# cursor.close()
-
-# cursor.execute("CREATE TABLE `sql7767099`.`trips` ( `trip_name` VARCHAR(30) , `trip_desc` VARCHAR(300),"
-#                " `trip_start` DATE, `trip_end` DATE , `trip_hotel` VARCHAR(20) ,"
-#                " `trip_cost` INT(7), `trip_activities` VARCHAR(300))")
-
-# cursor.execute("INSERT INTO `trips` (`trip_name`, `trip_desc`, `trip_start`, `trip_end`, `trip_hotel`, "
-#                "`trip_cost`, `trip_activities`)"
-#                " VALUES ('ibiza','nothing','2024/12/14', '2024/12/20', 'markiz', 500, 'polla')")
-# connection.commit()
 
 # cursor.execute("UPDATE `trips` SET `date` = DATE(`date`, '%d-%m-%Y')")
 
-# cursor.execute("DELETE FROM trips WHERE trip_start = '0000-00-00'")
-# connection.commit()
-
-# print(cursor.fetchall())
 # cursor.execute("DROP TABLE IF EXISTS trips")
 
 # cursor.execute("ALTER TABLE `sql7767099`.`buses` ADD bus_id VARCHAR(7) FIRST")
-
-# def crowdTrips(trips):
-# connection.commit()
-
 
 def createTables():
 
@@ -47,7 +30,6 @@ def createTables():
         "CREATE TABLE IF NOT EXISTS `sql7767099`.`bus_drivers` (`driver_id` INT AUTO_INCREMENT PRIMARY KEY, `driver_name` VARCHAR(25), `driver_lname` VARCHAR(25),"
         " `availability` BOOLEAN, `driver_salary` FLOAT(8,2), `cost_perTrip` FLOAT(8,2),"
          "`driver_hours` FLOAT(5,1),`favoured_destinations` VARCHAR(10))")
-
 
 # function to insert data into trips table
 def crowdTrips(trip_values):
@@ -202,7 +184,3 @@ def displayDriver(driver_id):
         print(driver)
     else:
         print("Driver not found.")
-
-
-
-
